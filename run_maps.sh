87ccf374103249b6f3863498deb4f8bd2222a2fa
anyu-yu <andrew.yu.x@gmail.com>
2022-03-06 16:11:15 -0800

    hashtag run with country and lang

--- run_maps.sh
+++ run_maps.sh
@@ -7,7 +7,12 @@ done
 ./src/reduce.py --input_paths outputs/geoTwitter*.lang --output_path=reduced.lang
 ./src/reduce.py --input_paths outputs/geoTwitter*.country --output_path=reduced.country
 
-var="'#코로나바이러스' '#コロナウイルス' '#冠状病毒' '#covid2019' '#covid-2019' '#covid19' '#covid-19' '#coronavirus' '#corona' '#virus' '#flu' '#sick' '#cough' '#sneeze' '#hospital'  '#nurse'  '#doctor'"
+var="#코로나바이러스 #コロナウイルス #冠状病毒 #covid2019 #covid-2019 #covid19 #covid-19 #coronavirus #corona #virus #flu #sick #cough #sneeze #hospital  #nurse  #doctor"
+
 for h in $var;
-do ./src/visualize.py --input_path=reduced.lang --key=$h | head > viz/$h;
+do ./src/visualize.py --input_path=reduced.lang --key=$h | head > viz/$h.lang;
+done
+
+for h in $var;
+do ./src/visualize.py --input_path=reduced.country --key=$h | head > viz/$h.country;
 done
